1b7011f244ad26c65dcf4337bf3a365389a1a1bd
Raphael Berger <raphaelmb@gmail.com>
2023-08-04 17:30:06 -0300

    add signin form

--- controllers/users.go
+++ controllers/users.go
@@ -2,13 +2,18 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/raphaelmb/go-lenslocked/models"
 )
 
 type Users struct {
 	Templates struct {
-		New Template
+		New    Template
+		SignIn Template
 	}
+	UserService *models.UserService
 }
 
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +24,23 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, data)
 }
 
+func (u *Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.SignIn.Execute(w, data)
+}
+
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Email: "+r.FormValue("email")+"\n")
-	fmt.Fprint(w, "Password: "+r.FormValue("password")+"\n")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "User created: %+v", user)
 }
--- main.go
+++ main.go
@@ -6,11 +6,23 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/raphaelmb/go-lenslocked/controllers"
+	"github.com/raphaelmb/go-lenslocked/models"
 	"github.com/raphaelmb/go-lenslocked/templates"
 	"github.com/raphaelmb/go-lenslocked/views"
 )
 
 func main() {
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	userService := models.UserService{
+		DB: db,
+	}
+
 	r := chi.NewRouter()
 
 	tmpl := views.Must(views.ParseFS(templates.FS, "home.tmpl.html", "tailwind.tmpl.html"))
@@ -22,10 +34,14 @@ func main() {
 	tmpl = views.Must(views.ParseFS(templates.FS, "faq.tmpl.html", "tailwind.tmpl.html"))
 	r.Get("/faq", controllers.FAQ(tmpl))
 
-	usersC := controllers.Users{}
+	usersC := controllers.Users{
+		UserService: &userService,
+	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.tmpl.html", "tailwind.tmpl.html"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.tmpl.html", "tailwind.tmpl.html"))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
